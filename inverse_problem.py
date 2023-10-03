ace01b08d5214e766d4bb0368808cd96eaca8985
jamalshabani <jjuma1992@gmail.com>
2023-10-03 19:46:14 -0400

    “Changes”

--- inverse_problem.py
+++ inverse_problem.py
@@ -3,11 +3,11 @@ from fenics_adjoint import *
 
 # Define the domain [-1, 1] x [-1, 1]
 n = 200
-mesh = RectangleMesh(-1, -1, 1, 1, n, n)
+mesh = UnitSquareMesh(n, n)
 
 # Define the function spaces
-V = FunctionSpace(mesh, "CG", degree = 1)
-W = FunctionSpace(mesh, "DG", degree = 0)
+V = FunctionSpace(mesh, "CG", 1)
+W = FunctionSpace(mesh, "DG", 0)
 
 # Define the functions
 u = Function(V, name = "Solution")
@@ -17,14 +17,23 @@ v = TestFunction(V)
 
 # Solve the forward model to create the tape
 F = (inner(grad(u), grad(v)) - m*v)*dx
-bc = DirichletBC(V, 0.0, "on_boundary")
-solve(F == 0, u, bc)
+
+def boundary(x, on_boundary):
+     return on_boundary
+bc = DirichletBC(V, Constant(0.0), boundary)
+
+solve(F == 0, u, bcs = bc)
 
 # Define the functional of interest
-u_d = exp(-1/(1-x[0]*x[0])-1/(1-x[1]*x[1]))
-J = Functional((0.5*inner(u - u_d, u - u_d))*dx)
+x, y = SpatialCoordinate(mesh)
+u_d = exp(-1/(1-x*x)-1/(1-y*y))
+ud = project(u_d, V)
+J = assemble((0.5*inner(u - u_d, u - u_d))*dx)
 
 # Define the reduced functional
-J_hat = ReducedFunctional(J, Control(m))
+J_hat = ReducedFunctional(J, SteadyParameter(m))
 # Solve the optimisation problem
 m_opt = minimize(J_hat, method = "L-BFGS-B", bounds = (0.0, 0.5),options = {"gtol": 1e-16, "ftol": 1e-16})
+File("problem/u.pvd") << u
+File("problem/ud.pvd") << ud
+File("problem/m.pvd") << m_opt
