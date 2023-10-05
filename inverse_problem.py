c7cb579e9bd5f4d93bc5f70f27f8e11cc3bf9445
jamalshabani <jjuma1992@gmail.com>
2023-10-05 10:22:34 -0400

    “Changes”

--- inverse_problem.py
+++ inverse_problem.py
@@ -1,3 +1,20 @@
+def parse():
+	import argparse
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-tao_type', '--tao_type', type = str, default = 'bncg', help = 'TAO algorithm type')
+	parser.add_argument('-tao_max_funcs', '--tao_max_funcs', type = int, default = 10000, help = 'TAO maximum functions evaluations')
+	parser.add_argument('-tao_monitor', '--tao_monitor', action = 'store_true', help = 'TAO monitor')
+	parser.add_argument('-tao_ls_monitor', '--tao_ls_monitor', action = 'store_true', help = 'TAO line search monitor')
+	parser.add_argument('-tao_ls_type', '--tao_ls_type', type = str, default = 'more-thuente', help = "TAO line search")
+	parser.add_argument('-tao_view', '--tao_view', action = 'store_true', help = "View convergence details")
+	parser.add_argument('-tao_max_it', '--tao_max_it', type = int, default = 100, help = 'Number of TAO iterations')
+	parser.add_argument('-o', '--output', type = str, default = 'output1', help = 'Output folder')
+	parser.add_argument('-n', '--meshsize', type = float, default = 100, help = 'Mesh size')
+	options = parser.parse_args()
+	return options
+
+options = parse()
+
 from firedrake import *
 from petsc4py import PETSc
 import time
@@ -7,4 +24,4 @@ start = time.time()
 
 mesh = UnitSquareMesh(100, 100)
 
-File("problem/mesh.pvd").write(mesh)
+V = FunctionSpace(mesh, 'CG', 1)
