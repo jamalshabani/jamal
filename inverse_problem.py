8d53ba8beecdd38fdc7be7653990cd2dadb42e6b
jamalshabani <jjuma1992@gmail.com>
2023-10-05 10:18:13 -0400

    “Changes”

--- inverse_problem.py
+++ inverse_problem.py
@@ -1,39 +1,10 @@
-from fenics import *
-from fenics_adjoint import *
+from firedrake import *
+from petsc4py import PETSc
+import time
+import numpy as np
 
-# Define the domain [-1, 1] x [-1, 1]
-n = 200
-mesh = UnitSquareMesh(n, n)
+start = time.time()
 
-# Define the function spaces
-V = FunctionSpace(mesh, "CG", 1)
-W = FunctionSpace(mesh, "DG", 0)
+mesh = UnitSquareMesh(100, 100)
 
-# Define the functions
-u = Function(V, name = "Solution")
-m = Function(W, name = "Control")
-v = TestFunction(V)
-
-
-# Solve the forward model to create the tape
-F = (inner(grad(u), grad(v)) - m*v)*dx
-
-def boundary(x, on_boundary):
-     return on_boundary
-bc = DirichletBC(V, Constant(0.0), boundary)
-
-solve(F == 0, u, bcs = bc)
-
-# Define the functional of interest
-x, y = SpatialCoordinate(mesh)
-u_d = exp(-1/(1-x*x)-1/(1-y*y))
-ud = project(u_d, V)
-J = assemble((0.5*inner(u - u_d, u - u_d))*dx)
-
-# Define the reduced functional
-J_hat = ReducedFunctional(J, SteadyParameter(m))
-# Solve the optimisation problem
-m_opt = minimize(J_hat, method = "L-BFGS-B", bounds = (0.0, 0.5),options = {"gtol": 1e-16, "ftol": 1e-16})
-File("problem/u.pvd") << u
-File("problem/ud.pvd") << ud
-File("problem/m.pvd") << m_opt
+File("problem/mesh.pvd").write(mesh)
