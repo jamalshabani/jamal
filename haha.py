23088d0000005c5dd6d31e2549c46fcfe37d210f
jamalshabani <jjuma1992@gmail.com>
2023-10-06 11:45:10 -0400

    “Changes”

--- haha.py
+++ haha.py
@@ -3,9 +3,9 @@ from fenics_adjoint import *
 
 mesh = UnitSquareMesh(50, 50)
 V = FunctionSpace(mesh, "CG", 1)
+vtkfile = File("problem/test.pvd")
 
 def u_target(V, t):
      data = Expression("16*x[0]*(x[0]-1)*x[1]*(x[1]-1)*sin(pi*t)", t = t, degree = 4)
-     u_tar = Function(V)
-     u_tar.interpolate(data, V)
+     u_tar = interpolate(data, V)
      return u_tar
