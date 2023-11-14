93ea2574f46230af910114a68b0fdfebfe1cad89
jamalshabani <jjuma1992@gmail.com>
2023-11-14 11:05:16 -0500

    “Changes”

--- run_time_obj_time_heat.py
+++ run_time_obj_time_heat.py
@@ -1,7 +1,7 @@
 import subprocess
 
 program_list = ["rm -rf test1", "rm -rf test2", "rm -rf test3",
-                "python3 time_obj_time_heat.py -tao_monitor -tao_max_it 500 -o test1 -k 1.0e-3 -e 1.0e-2 -ls 0.035 -lr 4.0"]
+                "python3 time_obj_time_heat.py -tao_monitor -tao_max_it 100 tao_ls_type unit -o test1 -k 1.0e-3 -e 1.0e-2 -ls 0.03 -lr 3.8"]
 
 
 i = 1
