108f900e2d2e8471d294fa9cf8ccf07745be5066
jamalshabani <jjuma1992@gmail.com>
2023-11-16 14:11:27 -0500

    “Changes”

--- test.py
+++ test.py
@@ -330,12 +330,11 @@ def FormObjectiveGradient(tao, x, G):
 			t += dt
 
 			rho_g.interpolate(2 * sin(2 * pi * t) * rho.sub(2))
+
+			R_heat_forward = s * w * dx + dt * k(rho) * inner(grad(s), grad(w)) * dx - (s_0 + dt * rho_g) * w * dx
 			solve(R_heat_forward == 0, s, bcs = bcss)
 			s_0.assign(s)
-			# s.assign(2 * sin(2 * pi * t) * s)
-			R_heat_forward = s * w * dx + dt * k(rho) * inner(grad(s), grad(w)) * dx - (s_0 + dt * rho_g) * w * dx
 
-			# multiply stimulus by a factor
 
 			# Step 2: Solve forward PDE
 			solve(R_fwd_s == 0, u, bcs = bcs)
