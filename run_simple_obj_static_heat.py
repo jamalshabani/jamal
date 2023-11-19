bdc781c5f6bfac95e922011fdb030ba0026d3315
jamalshabani <jjuma1992@gmail.com>
2023-11-19 18:59:00 -0500

    “Changes”

--- run_simple_obj_static_heat.py
+++ run_simple_obj_static_heat.py
@@ -1,7 +1,11 @@
 import subprocess
 
 program_list = ["rm -rf test1", "rm -rf test2", "rm -rf test3",
-                "python3 simple_obj_static_heat.py -m trajectory.msh -tao_monitor -tao_max_it 5000"]
+                "python3 simple_obj_static_heat.py -o test1 -tao_monitor -ls 0.055 -lr 5.0 -tao_max_it 5000",
+                "python3 simple_obj_static_heat.py -o test2 -tao_monitor -ls 0.06 -lr 5.0 -tao_max_it 5000",
+                "python3 simple_obj_static_heat.py -o test3 -tao_monitor -ls 0.065 -lr 5.0 -tao_max_it 5000",
+                "python3 simple_obj_static_heat.py -o test4 -tao_monitor -ls 0.07 -lr 5.0 -tao_max_it 5000",
+                "python3 simple_obj_static_heat.py -o test5 -tao_monitor -ls 0.075 -lr 5.0 -tao_max_it 5000"]
 
 
 i = 1
