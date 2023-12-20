5189ec1acbe8b02c3ef84122df017274de3e6fc7
jamalshabani <jjuma1992@gmail.com>
2023-12-20 14:31:39 -0500

    “Changes”

--- run_circle.py
+++ run_circle.py
@@ -1,9 +1,9 @@
 import subprocess
 
 program_list = ["rm -rf test1", "rm -rf test2", "rm -rf test3",
-                "python3 circle.py -tao_monitor -tao_max_it 2000 -tao_ls_type armijo -o test1 -k 1.0e-3 -e 1.0e-2 -ls 0.02 -lr 3.0",
-                "python3 circle.py -tao_monitor -tao_max_it 100 -tao_ls_type armijo -o test2 -k 1.0e-3 -e 1.0e-2 -ls 0.025 -lr 3.0",
-                "python3 circle.py -tao_monitor -tao_max_it 100 -tao_ls_type armijo -o test3 -k 1.0e-3 -e 1.0e-2 -ls 0.03 -lr 3.0"]
+                "python3 circle.py -tao_monitor -tao_max_it 100 -tao_ls_type armijo -o test1 -k 1.0e-3 -e 1.0e-2 -ls 0.02 -lr 6.0",
+                "python3 circle.py -tao_monitor -tao_max_it 100 -tao_ls_type armijo -o test2 -k 1.0e-3 -e 1.0e-2 -ls 0.02 -lr 7.0",
+                "python3 circle.py -tao_monitor -tao_max_it 100 -tao_ls_type armijo -o test3 -k 1.0e-3 -e 1.0e-2 -ls 0.02 -lr 8.0"]
 
 
 i = 1
